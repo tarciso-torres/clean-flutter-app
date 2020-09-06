502af3926662458a82c12df1b51695014cd1e01a
Tarciso Torres <tarcisoftorres@gmail.com>
2020-09-06 12:26:14 -0300

    ensure HttpAdapter returns null if post returns 204

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -48,9 +48,19 @@ void main() {
   }); 
 
   group('post', () {
+
+    PostExpectation mockRequest() => 
+       when(client.post(any, body: anyNamed('body'), headers: anyNamed('headers')));
+    
+    void mockResponse(int statusCode, {String body = '{"any_key": "any_value"}'}) {
+      mockRequest().thenAnswer((_) async => Response(body, statusCode));
+    }
+
+    setUp(() {
+      mockResponse(200);
+    });
+
     test('Should call post with correct values', () async {
-      when(client.post(any, body: anyNamed('body'), headers: anyNamed('headers')))
-        .thenAnswer((_) async => Response('{"any_key":"any_value"}', 200));
       await sut.request(url: url, method: 'post', body: {'any_key': 'any_value'});
 
       verify(client.post(
@@ -64,8 +74,6 @@ void main() {
     });
 
     test('Should call post withoud body', () async {
-      when(client.post(any, body: anyNamed('body'), headers: anyNamed('headers')))
-        .thenAnswer((_) async => Response('{"any_key":"any_value"}', 200));
       await sut.request(url: url, method: 'post');
 
       verify(client.post(
@@ -75,8 +83,6 @@ void main() {
     });
 
     test('Should return data if post returns 200', () async {
-      when(client.post(any, headers: anyNamed('headers')))
-        .thenAnswer((_) async => Response('{"any_key":"any_value"}', 200));
 
       final response = await sut.request(url: url, method: 'post');
 
@@ -84,12 +90,19 @@ void main() {
     });
 
     test('Should return null if post returns 200 with no data', () async {
-      when(client.post(any, headers: anyNamed('headers')))
-        .thenAnswer((_) async => Response('{"any_key":"any_value"}', 200));
+      mockResponse(200, body: '');
 
       final response = await sut.request(url: url, method: 'post');
 
-      expect(response, {'any_key': 'any_value'});
+      expect(response, null);
+    });
+
+    test('Should return null if post returns 204', () async {
+      mockResponse(204, body: '');
+
+      final response = await sut.request(url: url, method: 'post');
+
+      expect(response, null);
     });
 
   });
