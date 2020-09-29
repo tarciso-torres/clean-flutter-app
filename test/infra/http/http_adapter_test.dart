506643838080deb6fe889bd3d8b84b20fc270d42
Tarciso Torres <tarcisoftorres@gmail.com>
2020-09-29 17:50:31 -0300

    ensure HttpAdapter throws UnauthorizedError on 401

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -103,6 +103,14 @@ void main() {
       expect(future, throwsA(HttpError.serverError));
     });
 
+    test('Should return UnauthorizedError if post returns 401', () async {
+      mockResponse(401);
+
+      final future =  sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.unauthorized));
+    });
+
   });
   
 }
