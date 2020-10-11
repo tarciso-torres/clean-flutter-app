2696cf01fa28c3986c6a33bb3fd03f9584307acf
Tarciso Torres <tarcisoftorres@gmail.com>
2020-10-11 14:02:00 -0300

    ensure StreamLoginPresenter emits correct events on InvalidCredentialsError

--- lib/domain/helpers/domain_error.dart
+++ lib/domain/helpers/domain_error.dart
@@ -1,4 +1,15 @@
+import 'package:ForDev/domain/helpers/helpers.dart';
+
 enum DomainError {
   unexpected,
   invalidCredentials
+}
+
+extension DomainErrorExtension on DomainError {
+  String get description {
+    switch(this) {
+      case DomainError.invalidCredentials: return 'Credenciais inválidas.';
+      default: '';
+    }
+  }
 }
--- lib/presentation/presenters/stream_login_presenter.dart
+++ lib/presentation/presenters/stream_login_presenter.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
 
+import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 
+import '../../domain/helpers/helpers.dart';
 import '../../domain/usecases/usecases.dart';
 
 import '../protocols/protocols.dart';
@@ -11,6 +13,7 @@ class LoginState {
   String password;
   String emailError;
   String passwordError;
+  String mainError;
   bool isLoading = false;
 
   bool get isFormValid => emailError == null
@@ -29,6 +32,7 @@ class StreamLoginPresenter {
 
   Stream<String> get emailErrorStream => _controller.stream.map((state) => state.emailError).distinct();
   Stream<String> get passwordErrorStream => _controller.stream.map((state) => state.passwordError).distinct();
+  Stream<String> get mainErrorStream => _controller.stream.map((state) => state.mainError).distinct();
   Stream<bool> get isFormValidStream => _controller.stream.map((state) => state.isFormValid).distinct();
   Stream<bool> get isLoadingStream => _controller.stream.map((state) => state.isLoading).distinct();
 
@@ -51,7 +55,11 @@ class StreamLoginPresenter {
   Future<void> auth() async {
     _state.isLoading = true;
     update();
-    await authentication.auth(AuthenticationParams(email: _state.email, secret: _state.password));
+    try{
+      await authentication.auth(AuthenticationParams(email: _state.email, secret: _state.password));
+    } on DomainError catch (error){
+      _state.mainError = error.description;
+    }
     _state.isLoading = false;
     update();
   }
