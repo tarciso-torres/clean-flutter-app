2663e221fb82f9f3c29dd170204b70661584cb11
Tarciso Torres <tarcisoftorres@gmail.com>
2020-10-15 17:18:34 -0300

    add SaveCurrentAccount use case

--- lib/data/usecases/authentication/remote_authentication.dart
+++ lib/data/usecases/authentication/remote_authentication.dart
@@ -1,11 +1,11 @@
 import 'package:meta/meta.dart';
 
-import '../../domain/entities/entities.dart';
-import '../../domain/helpers/helpers.dart';
-import '../../data/models/models.dart';
-import '../../domain/usecases/usecases.dart';
+import '../../../domain/entities/entities.dart';
+import '../../../domain/helpers/helpers.dart';
+import '../../models/models.dart';
+import '../../../domain/usecases/usecases.dart';
 
-import '../http/http.dart';
+import '../../http/http.dart';
 
 class RemoteAuthentication implements Authentication{
   final HttpClient httpClient;
