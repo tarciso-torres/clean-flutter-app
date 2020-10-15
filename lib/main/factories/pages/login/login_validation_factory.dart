21c537528b93860a9f6f8cfc4f55841803fa3ff3
Tarciso Torres <tarcisoftorres@gmail.com>
2020-10-15 14:20:30 -0300

    Add ValidationBuilder

--- lib/main/factories/pages/login/login_validation_factory.dart
+++ lib/main/factories/pages/login/login_validation_factory.dart
@@ -2,14 +2,15 @@ import '../../../../presentation/protocols/protocols.dart';
 import '../../../../validation/validators/validators.dart';
 import '../../../../validation/protocols/protocols.dart';
 
+import '../../../builders/validation_builder.dart';
+
 Validation makeLoginValidation() {
   return ValidationComposite(makeLoginValidations());
 }
 
 List<FieldValidation> makeLoginValidations() {
   return [
-    RequiredFieldValidation('email'),
-    EmailValidation('email'),
-    RequiredFieldValidation('password')
+    ...ValidationBuilder.field('email').required().email().build(),
+    ...ValidationBuilder.field('password').required().build()
   ];
 }
