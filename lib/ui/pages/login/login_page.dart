7adcb86f7165278b7accc03a5c734e526932a82f
Tarciso Torres <tarcisoftorres@gmail.com>
2020-10-15 14:00:51 -0300

    hide keyboard on tap

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -6,7 +6,6 @@ import 'components/components.dart';
 import 'login_presenter.dart';
 
 class LoginPage extends StatefulWidget {
-
   final LoginPresenter presenter;
 
   const LoginPage({Key key, this.presenter}) : super(key: key);
@@ -16,6 +15,12 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  void _hideKeyboard() {
+    final currentFocus = FocusScope.of(context);
+    if(!currentFocus.hasPrimaryFocus) {
+      currentFocus.unfocus();
+    }
+  }
 
   @override
   void dispose() {
@@ -26,56 +31,58 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Builder(
-          builder: (context) {
-            widget.presenter.isLoadingStream.listen((isLoading) { 
-            if(isLoading) {
+      body: Builder(
+        builder: (context) {
+          widget.presenter.isLoadingStream.listen((isLoading) {
+            if (isLoading) {
               showLoading(context);
             } else {
               hideLoading(context);
             }
-            });
+          });
 
-            widget.presenter.mainErrorStream.listen((error) {
-              if(error != null) {
-                showErrorMessage(context: context, error: error);
-              }
-            });
-            return SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                LoginHeader(),
+          widget.presenter.mainErrorStream.listen((error) {
+            if (error != null) {
+              showErrorMessage(context: context, error: error);
+            }
+          });
+          return GestureDetector(
+            onTap: _hideKeyboard,
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  LoginHeader(),
                   HeadLine1(text: 'login'),
                   Padding(
                     padding: const EdgeInsets.all(32.0),
                     child: Provider(
-                        create: (_) => widget.presenter,
-                        child: Form(
-                          child: Column(
-                            children: <Widget>[
-                              EmailInput(),
-                              Padding(
-                                padding: const EdgeInsets.only(top: 8, bottom: 32),
-                                child: PasswordInput(),
-                              ),
-                              LoginButton(),
-                              FlatButton.icon(
+                      create: (_) => widget.presenter,
+                      child: Form(
+                        child: Column(
+                          children: <Widget>[
+                            EmailInput(),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(top: 8, bottom: 32),
+                              child: PasswordInput(),
+                            ),
+                            LoginButton(),
+                            FlatButton.icon(
                                 onPressed: () {},
                                 icon: Icon(Icons.person),
                                 label: Text('Criar Conta'))
-                            ],
-                          ),),
+                          ],
+                        ),
                       ),
-                    )
+                    ),
+                  )
                 ],
               ),
-            );
-          },
-        ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
-
-
-
