acc771b920c71c82113bf705277bcc7c614689e4
Tarciso Torres <tarcisoftorres@gmail.com>
2020-10-19 12:54:02 -0300

    remove hide loading in success case

--- lib/presentation/presenters/getx_login_presenter.dart
+++ lib/presentation/presenters/getx_login_presenter.dart
@@ -50,14 +50,14 @@ class GetxLoginPresenter implements LoginPresenter {
   }
 
   Future<void> auth() async {
-    _isLoading.value = true;
     try {
+      _isLoading.value = true;
       final account = await authentication.auth(AuthenticationParams(email: _email, secret: _password));
       await saveCurrentAccount.save(account);
     } on DomainError catch (error) {
       _mainError.value = error.description;
+      _isLoading.value = false;
     }
-    _isLoading.value = false;
   }
 
   void dispose() {}
