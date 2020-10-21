b08ed12cb10d0dca456761c715d912dfee620edb
Tarciso Torres <tarcisoftorres@gmail.com>
2020-10-21 12:53:54 -0300

    Move the SUT creation to the setup method

--- test/data/usecases/load_current_account/local_load_current_account_test.dart
+++ test/data/usecases/load_current_account/local_load_current_account_test.dart
@@ -19,9 +19,16 @@ abstract class FetchSecureCacheStorage {
 class FetchSecureCacheStorageSpy extends Mock implements FetchSecureCacheStorage {}
 
 void main() {
+
+  FetchSecureCacheStorageSpy fetchSecureCacheStorage;
+  LocalLoadCurrentAccount sut;
+
+  setUp(() {
+    fetchSecureCacheStorage = FetchSecureCacheStorageSpy();
+    sut = LocalLoadCurrentAccount(fetchSecureCacheStorage: fetchSecureCacheStorage);
+  });
+
   test('Should call FetchSecureCacheStorage with correct value', () async {
-    final fetchSecureCacheStorage = FetchSecureCacheStorageSpy();
-    final sut = LocalLoadCurrentAccount(fetchSecureCacheStorage: fetchSecureCacheStorage);
 
     await sut.load();
 
