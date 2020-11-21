463c958a9cc2abb8ae2d7c4b352f7975d8dce8c3
Tarciso Torres <tarcisoftorres@gmail.com>
2020-11-21 16:32:20 -0300

    ensure RemoteAddAccount throws UnexpectedError on 500

--- test/data/usecases/add_account/remote_add_account_test.dart
+++ test/data/usecases/add_account/remote_add_account_test.dart
@@ -67,4 +67,12 @@ void main() {
     expect(future, throwsA(DomainError.unexpected));
   });
 
+  test('Should throw UnexpectedError if HttpClient returns 500', () async {
+    mockHttpError(HttpError.serverError);
+
+    final future = sut.add(params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
 }
