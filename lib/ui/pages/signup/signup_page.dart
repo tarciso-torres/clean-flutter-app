6fb36a1fb305a051b3f6ed1146c3757d1e8b5c4d
Tarciso Torres <tarcisoftorres@gmail.com>
2020-11-25 18:10:26 -0300

    ensure SignUpPage calls Validation on any field change

--- lib/ui/pages/signup/signup_page.dart
+++ lib/ui/pages/signup/signup_page.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 
 import '../../components/components.dart';
 import '../../helpers/helpers.dart';
 import 'components/components.dart';
+import 'signup_presenter.dart';
 
 class SignUpPage extends StatelessWidget {
+  final SignUpPresenter presenter;
+
+  const SignUpPage({this.presenter});
 
 
   @override
@@ -29,27 +34,30 @@ class SignUpPage extends StatelessWidget {
                   HeadLine1(text: R.strings.addAccount),
                   Padding(
                     padding: const EdgeInsets.all(32.0),
-                    child: Form(
-                      child: Column(
-                        children: <Widget>[
-                          NameInput(),
-                          Padding(
-                            padding:
-                                const EdgeInsets.symmetric(vertical: 8),
-                            child: EmailInput(),
-                          ),
-                          PasswordInput(),
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(top: 8, bottom: 32),
-                            child: PasswordConfirmationInput(),
-                          ),
-                          SignUpButton(),
-                          FlatButton.icon(
-                              onPressed: () {},
-                              icon: Icon(Icons.exit_to_app),
-                              label: Text(R.strings.login))
-                        ],
+                    child: Provider(
+                      create: (_) => presenter,
+                      child: Form(
+                        child: Column(
+                          children: <Widget>[
+                            NameInput(),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 8),
+                              child: EmailInput(),
+                            ),
+                            PasswordInput(),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(top: 8, bottom: 32),
+                              child: PasswordConfirmationInput(),
+                            ),
+                            SignUpButton(),
+                            FlatButton.icon(
+                                onPressed: () {},
+                                icon: Icon(Icons.exit_to_app),
+                                label: Text(R.strings.login))
+                          ],
+                        ),
                       ),
                     ),
                   )
--- requirements/checklist/pages/signup/signup_page.md
+++ requirements/checklist/pages/signup/signup_page.md
@@ -4,16 +4,16 @@
 01. ✅ Os campos devem começar sem exibir mensagem de erro
 02. ✅ O botão de criar conta deve começar desabilitado0
 03.  Após digitar algo em um campo, a mensagem de erro só deve sumir se o campo for válido
-04.  Validar email quando o usuário digitar no campo
+04. ✅ Validar email quando o usuário digitar no campo
 05.  Mostrar mensagem de erro se o email for inválido
 06.  Remover mensagem de erro se o email for válido
-07.  Validar senha quando o usuário digitar no campo
+07. ✅ Validar senha quando o usuário digitar no campo
 08.  Mostrar mensagem de erro se a senha for inválida
 09.  Remover mensagem de erro se a senha for válida
-10.  Validar nome quando o usuário digitar no campo
+10. ✅ Validar nome quando o usuário digitar no campo
 11.  Mostrar mensagem de erro se o nome for inválido
 12.  Remover mensagem de erro se o nome for válido
-13.  Validar confirmação de senha quando o usuário digitar no campo
+13. ✅ Validar confirmação de senha quando o usuário digitar no campo
 14.  Mostrar mensagem de erro se a confirmação de senha for inválida
 15.  Remover mensagem de erro se a confirmação de senha for válida
 16.  Habilitar o botão de criar conta se todos os campos forem válidos
