cc60d1302f60db2251847aff5711ab57917bec69
Tarciso Torres <tarcisoftorres@gmail.com>
2021-01-29 18:44:14 -0300

    ensure RemoteLoadSurveys throws UnexpectedError on 500

--- test/data/usecases/load_surveys/remote_load_surveys_test.dart
+++ test/data/usecases/load_surveys/remote_load_surveys_test.dart
@@ -111,4 +111,22 @@ void main() {
 
   }); 
 
+  test('Should throw UnexpectedError if HttpClient returns 500', () async {
+    mockHttpError(HttpError.serverError);
+
+    final future = sut.load();
+
+    expect(future, throwsA(DomainError.unexpected));
+
+  }); 
+
+  test('Should throw AccessDeniedError if HttpClient returns 403', () async {
+    mockHttpError(HttpError.serverError);
+
+    final future = sut.load();
+
+    expect(future, throwsA(DomainError.accessDenied));
+
+  });
+
 }
