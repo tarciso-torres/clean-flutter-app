c70f920caf0e5e7154c939d4d9f7ab111bd76b2e
Tarciso Torres <tarcisoftorres@gmail.com>
2021-02-01 18:18:01 -0300

    test: ensure HttpAdapter throws BadRequest on 400

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -205,6 +205,14 @@ void main() {
       expect(response, null);
     });
 
+    test('Should return BadRequestError if get returns 400', () async {
+      mockResponse(400, body: '');
+
+      final future =  sut.request(url: url, method: 'get');
+
+      expect(future, throwsA(HttpError.badRequest));
+    });
+
   });
   
 }
