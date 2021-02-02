cb7a69a8dbd12f47e6215b8a303b6e0686b5e8bc
Tarciso Torres <tarcisoftorres@gmail.com>
2021-02-02 17:17:52 -0300

    chore: ensure SurveysPage handles loading correctly

--- lib/ui/pages/surveys/surveys_page.dart
+++ lib/ui/pages/surveys/surveys_page.dart
@@ -3,31 +3,44 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import 'components/components.dart';
+import '../../components/components.dart';
 import '../../helpers/helpers.dart';
+import 'components/components.dart';
 
 class SurveysPage extends StatelessWidget {
   final SurveysPresenter presenter;
 
   SurveysPage(this.presenter);
+
   @override
   Widget build(BuildContext context) {
     presenter.loadData();
     return Scaffold(
       appBar: AppBar(title: Text(R.strings.surveys),),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 20),
-        child: CarouselSlider(
-          options: CarouselOptions(
-            enlargeCenterPage: true,
-            aspectRatio: 1
-          ),
-          items: [
-            SurveyItem(),
-            SurveyItem(),
-            SurveyItem(),
-          ]
-          ,),
+      body: Builder(
+        builder: (context) {
+          presenter.isLoadingStream.listen((isLoading) {
+            if(isLoading == true) {
+              showLoading(context);
+            } else {
+              hideLoading(context);
+            }
+           });
+          return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20),
+          child: CarouselSlider(
+            options: CarouselOptions(
+              enlargeCenterPage: true,
+              aspectRatio: 1
+            ),
+            items: [
+              SurveyItem(),
+              SurveyItem(),
+              SurveyItem(),
+            ]
+            ,),
+         );
+        }
       ),
     );
   }
