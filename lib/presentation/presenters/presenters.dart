2498fc37b4c7cb8e1f26cf852551746e67373cc6
Tarciso Torres <tarcisoftorres@gmail.com>
2021-02-05 13:36:52 -0300

    refactor: move GetxSurveysPresenter to its own file

--- lib/presentation/presenters/presenters.dart
+++ lib/presentation/presenters/presenters.dart
@@ -1,4 +1,5 @@
 export './getx_login_presenter.dart';
+export './getx_serveys_presenter.dart';
 export './getx_signup_presenter.dart';
 export './getx_splash_presenter.dart';
 export './stream_login_presenter.dart';
