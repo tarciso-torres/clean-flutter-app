a2ea2513bddf54fe2b50b24a6cf18f6ec7a67a68
Tarciso Torres <tarcisoftorres@gmail.com>
2021-02-25 17:18:50 -0300

    refactor: move LocalLoadSurveys to its own file

--- test/data/usecases/load_surveys/local_load_surveys_test.dart
+++ test/data/usecases/load_surveys/local_load_surveys_test.dart
@@ -1,32 +1,14 @@
 
 import 'package:ForDev/data/cache/cache.dart';
-import 'package:ForDev/data/models/models.dart';
+import 'package:ForDev/data/usecases/usecases.dart';
+
 import 'package:ForDev/domain/entities/entities.dart';
 import 'package:ForDev/domain/helpers/domain_error.dart';
 import 'package:faker/faker.dart';
-import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
-class LocalLoadSurveys {
-  final FetchCacheStorage fetchCacheStorage;
 
-  LocalLoadSurveys({
-    @required this.fetchCacheStorage
-  });
-
-  Future<List<SurveyEntity>> load() async {
-    try {
-      final data = await fetchCacheStorage.fetch('surveys');
-      if(data?.isEmpty != false) {
-        throw Exception();
-      }
-      return data.map<SurveyEntity>((json) => LocalSurveyModel.fromJson(json).toEntity()).toList();
-    } catch(error) {
-      throw DomainError.unexpected;
-    }
-  }
-}
 
 class FetchCacheStorageSpy extends Mock implements FetchCacheStorage{}
 
