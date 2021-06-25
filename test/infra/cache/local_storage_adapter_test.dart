de7296a035a3dc1d6277df7b36a5707e1e41120d
Tarciso Torres <tarcisoftorres@gmail.com>
2021-06-25 16:10:12 -0300

    test: ensure LocalStorageAdapter returns same value as cache on fetch

--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -68,11 +68,25 @@ void main() {
   });
 
   group('fetch', () {
+    String result;
+
+    void mockFetch() =>
+        when(localStorage.getItem(any)).thenAnswer((_) => result);
+
+    setUp(() {
+      mockFetch();
+    });
+
     test('Should call LocalStorage with correct values', () async {
       await sut.fetch(key);
 
-      verify(localStorage.deleteItem(key)).called(1);
+      verify(localStorage.getItem(key)).called(1);
     });
 
+    test('Should return same value as localStorage', () async {
+      final data = await sut.fetch(key);
+
+      expect(data, result);
+    });
   });
 }
