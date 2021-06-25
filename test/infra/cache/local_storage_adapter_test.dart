6797a3618ed5585091c48eecdc757b73c90edad9
Tarciso Torres <tarcisoftorres@gmail.com>
2021-06-25 08:20:54 -0300

    test: ensure LocalStorageAdapter throws if deleteItem throws

--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -24,6 +24,9 @@ void main() {
   LocalStorageSpy localStorage;
   LocalStorageAdapter sut;
 
+  void mockDeleteItemError() =>
+    when(localStorage.deleteItem(any)).thenThrow(Exception());
+
   setUp(() {
     key = faker.randomGenerator.string(5);
     value = faker.randomGenerator.string(50);
@@ -37,5 +40,13 @@ void main() {
 
     verify(localStorage.deleteItem(key)).called(1);
     verify(localStorage.setItem(key, value)).called(1);
+  });
+
+  test('Should throw if deleteItem throws', () async {
+    mockDeleteItemError();
+    
+    final future =  sut.save(key: key, value: value);
+
+    expect(future, throwsA(TypeMatcher<Exception>()));
   });
 }
