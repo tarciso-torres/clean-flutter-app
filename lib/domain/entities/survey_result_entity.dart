7c047ca3bcabc41bb7fc840de3d87942fe7684f6
Tarciso Torres <tarcisoftorres@gmail.com>
2021-06-29 15:08:58 -0300

    chore: add SurveyResult Entity

---
+++ lib/domain/entities/survey_result_entity.dart
@@ -0,0 +1,14 @@
+import 'package:ForDev/domain/entities/entities.dart';
+import 'package:meta/meta.dart';
+
+class SurveyResultEntity{
+  final String surveyId;
+  final String question;
+  final List<SurveyAnswerEntity> answers;
+
+  SurveyResultEntity({ 
+    @required this.surveyId, 
+    @required this.question, 
+    @required this.answers 
+  });
+}
