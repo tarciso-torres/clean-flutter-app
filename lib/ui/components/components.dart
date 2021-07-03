c218b11ecdb54e379912f6654d7fc4105da60a2d
Tarciso Torres <tarcisoftorres@gmail.com>
2021-07-03 12:18:41 -0300

    refactor: move ReloadScreen component to its own file

--- lib/ui/components/components.dart
+++ lib/ui/components/components.dart
@@ -2,4 +2,5 @@ export './app_theme.dart';
 export './login_header.dart';
 export './head_line1.dart';
 export './spinner_dialog.dart';
+export './reload_screen.dart';
 export './show_error_message.dart';
