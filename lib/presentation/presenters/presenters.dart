89599edf56a91ad9b8ca35a9f84ed4d73ac705e9
Tarciso Torres <tarcisoftorres@gmail.com>
2021-07-08 07:46:52 -0300

    test: add GetxSurveyResultPresenter with tests

--- lib/presentation/presenters/presenters.dart
+++ lib/presentation/presenters/presenters.dart
@@ -1,4 +1,5 @@
 export './getx_login_presenter.dart';
+export './getx_servey_result_presenter.dart';
 export './getx_serveys_presenter.dart';
 export './getx_signup_presenter.dart';
 export './getx_splash_presenter.dart';
