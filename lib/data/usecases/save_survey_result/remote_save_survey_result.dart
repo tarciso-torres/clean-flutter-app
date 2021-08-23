b50661f03a2ef0a20fd7974f214f5b90bec3a54e
Tarciso Torres <tarcisoftorres@gmail.com>
2021-08-22 21:20:23 -0300

    test: ensure RemoteSaveSurveyResult throws DomainError on success but with invalid data

--- lib/data/usecases/save_survey_result/remote_save_survey_result.dart
+++ lib/data/usecases/save_survey_result/remote_save_survey_result.dart
@@ -1,8 +1,8 @@
-import 'package:ForDev/domain/usecases/usecases.dart';
 import 'package:meta/meta.dart';
 
 import '../../../domain/entities/entities.dart';
 import '../../../domain/helpers/helpers.dart';
+import '../../../domain/usecases/usecases.dart';
 
 import '../../http/http.dart';
 import '../../models/models.dart';
--- lib/domain/usecases/save_survey_result.dart
+++ lib/domain/usecases/save_survey_result.dart
@@ -3,5 +3,5 @@ import 'package:meta/meta.dart';
 import '../entities/entities.dart';
 
 abstract class SaveSurveyResult {
-  Future<SurveyResultEntity> save ({@required String surveyId});
+  Future<SurveyResultEntity> save ({@required String answer});
 }
